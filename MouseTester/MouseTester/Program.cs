c54bf37ddf89daee2556b26d58f57b9775c90262
valleyofdoom <173058957+valleyofdoom@users.noreply.github.com>
2025-01-20 15:38:47 +0000

    Update version

--- MouseTester/MouseTester/Program.cs
+++ MouseTester/MouseTester/Program.cs
@@ -7,7 +7,7 @@ namespace MouseTester
 {
     static class Program
     {
-        public static string version = "1.7.0";
+        public static string version = "1.7.1";
 
         /// <summary>
         /// The main entry point for the application.
